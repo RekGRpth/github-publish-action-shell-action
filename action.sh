3cf215336b7dde12294e464373b431d2c6413523
RekGRpth <rekgrpth@gmail.com>
2021-08-12 10:33:05 +0500

    up

--- action.sh
+++ action.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -eux
-hub api --method DELETE "repos/${GITHUB_REPOSITORY}/git/refs/tags/${GITHUB_REF##*/}" | jq .
-RELEASE_ID="$(hub api --method GET "repos/${GITHUB_REPOSITORY}/releases/tags/${GITHUB_REF##*/}" | jq .id)"
+hub api --method DELETE "repos/${GITHUB_REPOSITORY}/git/refs/tags/${INPUTS_TAG:-${GITHUB_REF##*/}}" | jq .
+RELEASE_ID="$(hub api --method GET "repos/${GITHUB_REPOSITORY}/releases/tags/${INPUTS_TAG:-${GITHUB_REF##*/}}" | jq .id)"
 hub api --method DELETE "repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}" | jq .
-hub api --method POST "repos/${GITHUB_REPOSITORY}/releases" --field "tag_name=${GITHUB_REF##*/}" | jq .
+hub api --method POST "repos/${GITHUB_REPOSITORY}/releases" --field "tag_name=${INPUTS_TAG:-${GITHUB_REF##*/}}" | jq .

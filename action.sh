f29efe99c032efd64ab5f49b23833a6c148f22ea
Georgy Shelkovy <rekgrpth@gmail.com>
2025-06-17 08:20:34 +0500

    fix

--- action.sh
+++ action.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -euxo pipefail
-RELEASE_ID="$(gh api --method GET "repos/${GITHUB_REPOSITORY}/releases/tags/${INPUTS_TAG:-${GITHUB_REF##*/}-tag}" | jq .id)"
-if [ -n "$RELEASE_ID" ]; then
+if [ gh api --method GET "repos/${GITHUB_REPOSITORY}/releases/tags/${INPUTS_TAG:-${GITHUB_REF##*/}-tag}" ]; then
+    RELEASE_ID="$(gh api --method GET "repos/${GITHUB_REPOSITORY}/releases/tags/${INPUTS_TAG:-${GITHUB_REF##*/}-tag}" | jq .id)"
     gh api --method DELETE "repos/${GITHUB_REPOSITORY}/git/refs/tags/${INPUTS_TAG:-${GITHUB_REF##*/}-tag}" | jq .
     gh api --method DELETE "repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}" | jq .
 fi

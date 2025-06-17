1735d2f9cfe0d5ee90b18154f4d5bad374e45fc9
Georgy Shelkovy <rekgrpth@gmail.com>
2025-06-17 08:04:37 +0500

    pipefail

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eux
+set -euxo pipefail
 gh api --method DELETE "repos/${GITHUB_REPOSITORY}/git/refs/tags/${INPUTS_TAG:-${GITHUB_REF##*/}-tag}" | jq .
 RELEASE_ID="$(gh api --method GET "repos/${GITHUB_REPOSITORY}/releases/tags/${INPUTS_TAG:-${GITHUB_REF##*/}-tag}" | jq .id)"
 gh api --method DELETE "repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}" | jq .
